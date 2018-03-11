8215e2a227f53911724a7729d955d790d1d0c4e1
Mattemyo <matias97marti@gmail.com>
2018-03-11 20:00:04 +0100

    set up project

---
+++ ClientApp/src/App.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import { Route } from 'react-router';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Counter from './components/Counter';
+import FetchData from './components/FetchData';
+import BounceSwitch from './components/transitions/BounceSwitch';
+import './App.css';
+
+export default () => 
+  <Layout>
+    <BounceSwitch>
+      <Route exact path="/" component={Home} />
+      <Route path="/counter" component={Counter} />
+      <Route path="/fetchdata/:startDateIndex?" component={FetchData} />
+    </BounceSwitch>
+  </Layout>
+;
